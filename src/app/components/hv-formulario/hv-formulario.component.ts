79da58689d90464790a954e9ae3286f75231d8c1
Julian Henao DMS <julian.henao@dms.ms>
2022-08-16 13:46:03 -0500

    Añade pasos adicionales al stepper

--- src/app/components/hv-formulario/hv-formulario.component.ts
+++ src/app/components/hv-formulario/hv-formulario.component.ts
@@ -7,13 +7,29 @@ import {FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./hv-formulario.component.scss']
 })
 export class HvFormularioComponent implements OnInit {
+  isLinear = false;
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
   });
   secondFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
   });
-  isLinear = false;
+  thirdFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  fourthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  fifthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  sixthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  seventhFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
 
   constructor(private _formBuilder: FormBuilder) {}
 
